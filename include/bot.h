fecf89fe22fced00d957e3fe6a4c7a17e422b15f
kevz8 <kevz8811@gmail.com>
2025-08-22 17:29:00 -0700

    added bot struct

---
+++ include/bot.h
@@ -0,0 +1,15 @@
+#ifndef BOT_H
+#define BOT_H
+#include <utility>
+
+struct Bot {
+    std::pair<float, float> pos;
+    std::pair<float, float> vel;
+    float mass = 0.3f;
+    float dragCoeff = 0.2f;
+    float maxSpeed = 800.0f;
+};
+
+Bot bot;
+
+#endif
